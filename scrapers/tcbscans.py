ebc7ec53ec92704af09647ef6af0eafb813bb0bb
27bslash <greenpear68@yahoo.co.uk>
2022-06-05 16:48:22 +0100

    added seconds since,  to be closer to release date

--- scrapers/tcbscans.py
+++ scrapers/tcbscans.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import time
+import datetime
 
 
 class TcbScraper():
@@ -17,9 +18,12 @@ class TcbScraper():
         link_regex = r"[^\/]+$"
         title_regex = r".*(?=-chapter)"
         chapter_regex = r"(?<=chapter-)\d*\.?\d+"
-        with open('scrapers/test_pages/tcb.html', 'r', encoding="utf-8") as f:
-            text = f.read()
-        soup = BeautifulSoup(text, 'html.parser')
+        # with open('scrapers/test_pages/tcb.html', 'r', encoding="utf-8") as f:
+        #     text = f.read()
+        req = requests.get('https://onepiecechapters.com/')
+        if req.status_code != 200:
+            print('tcbscasn', req.status_code, 'broken')
+        soup = BeautifulSoup(req.text, 'html.parser')
         cards = soup.select('div[class*="bg-card"]')
         lst = []
         for card in cards:
@@ -31,23 +35,31 @@ class TcbScraper():
                 path = re.search(link_regex, url).group(0)
                 title = re.search(title_regex, path).group(0)
                 chapter = re.search(chapter_regex, path).group(0)
+
                 d['type'] = 'tcb'
-                d['time_updated'] = time.time()
+                d['time_updated'] = time.time()-self.seconds_since_update()
                 d['title'] = title
                 d['latest'] = chapter
                 d['domain'] = 'https://onepiecechapters.com/'
                 d['link'] = url
-                d['scansite'] = 'onepiecechapters'
-                # print(d)
+                d['scansite'] = 'tcbscans'
                 lst.append(d)
             except TypeError:
                 print(url)
                 continue
-        print(lst)
+        # print(lst)
         return lst
 
+    def seconds_since_update(self):
+        today = datetime.datetime.today().weekday()
+        days_since_thursday = 7 - (3 - today) % 7
+        if today == 2:
+            return 0
+        else:
+            return 86400 * days_since_thursday
+
     def __call__(self):
-        self.scrape()
+        return self.scrape()
 
 
 t = TcbScraper()
